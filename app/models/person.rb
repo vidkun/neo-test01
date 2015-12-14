25fea59480696f03a46dc2ab042058c4eab52a49
Ryan LeViseur <vidkun@users.noreply.github.com>
2015-12-14 14:27:19 -0600

    add a working query

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,4 +7,8 @@ class Person
   # p1.create_rel("related_to", p2, type: "ParentChild")
   has_many :out, :related_to, type: :related_to, model_class: :Person
   has_many :in, :people, type: :related_to
+
+  # p1.related_to(:n, :rel, rel_length: 2).query.unwind(r: :rel).with('DISTINCT r, n').where(r: { type: 'ParentChild' }).with('DISTINCT n').pluck(:n).each do |ppl|
+  #   puts ppl.names["fullText"]
+  # end
 end
